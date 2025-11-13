5e77e70641e2da888a4a6328e0b87edc954cbc69
Teoh TeikToe <teohteiktoe@gmail.com>
2025-11-13 11:06:13 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,37 +11,37 @@ if os.path.exists('.env'):
 
 client = Groq()
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route("/",methods=["GET","POST"])
+@application.route("/",methods=["GET","POST"])
 def index():
     return(render_template("index.html"))
 
-@app.route("/main",methods=["GET","POST"])
+@application.route("/main",methods=["GET","POST"])
 def main():
     return(render_template("main.html"))
 
-@app.route("/dbs",methods=["GET","POST"])
+@application.route("/dbs",methods=["GET","POST"])
 def dbs():
     return(render_template("dbs.html"))
 
 
-@app.route("/dbs_prediction",methods=["GET","POST"])
+@application.route("/dbs_prediction",methods=["GET","POST"])
 def dbs_prediction():
     q = float(request.form.get("q"))
     model = joblib.load("DBS_SGD_model.pkl")
     r = model.predict([[q]])
     return(render_template("dbs_prediction.html",r=r))
 
-@app.route("/chatbot",methods=["GET","POST"])
+@application.route("/chatbot",methods=["GET","POST"])
 def chatbot():
     return(render_template("chatbot.html"))
 
-@app.route("/llama",methods=["GET","POST"])
+@application.route("/llama",methods=["GET","POST"])
 def llama():
     return(render_template("llama.html"))
 
-@app.route("/llama_result",methods=["GET","POST"])
+@application.route("/llama_result",methods=["GET","POST"])
 def llama_result():
     q = request.form.get("q")
     r = client.chat.completions.create(
@@ -53,4 +53,4 @@ def llama_result():
     return(render_template("llama_result.html",r=r))
 
 if __name__ == "__main__":
-    app.run()
+    application.run()
